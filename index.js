f52ec8b9af230b12998fd63b48da154e6467411f
grega-rotar <grega.rotar@etiam.si>
2026-01-15 15:47:10 +0100

    fix: faster startup

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   // First, run initial setup jobs
-  await initJobs();
+  initJobs();
 
   // Then, start the server
   app.listen(PORT, () => {
